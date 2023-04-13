0969743bf418f00dc185702d77bd04ed78ddf30b
Caleb Weinreb <calebsw@gmail.com>
2023-04-13 18:31:00 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", 'r', encoding='utf-8') as f:
 
 setuptools.setup(
     name='keypoint-moseq',
-    version='0.0.2',
+    version='0.0.3',
     author='Caleb Weinreb',
     author_email='calebsw@gmail.com',
     include_package_data=True,
