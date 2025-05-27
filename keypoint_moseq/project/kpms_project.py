61d153515e7c46d1f9329d1e787df403590add3f
John Jacoby <jrdjacoby@gmail.com>
2025-05-27 15:57:55 -0400

    committing before writing test examples

--- keypoint_moseq/project/kpms_project.py
+++ keypoint_moseq/project/kpms_project.py
@@ -3,6 +3,86 @@ import numpy as np
 from pathlib import Path
 from os import PathLike
 
+def _check_id_column_integrity(dataframe: pl.DataFrame, id_column: str):
+    """Check that a dataframe has an id column and no duplicates in it.
+
+    Parameters
+    ----------
+    dataframe: pl.DataFrame
+        The dataframe to check
+    id_column: str
+        The column to check for duplicates
+
+    Raises
+    ------
+    ValueError
+        Raised when dataframe does not have the id_column
+    ValueError
+        Raised when dataframe has duplicates in the id_column
+    """
+    if id_column not in dataframe.columns:
+        raise ValueError(f'Dataframe must have column id_column "{id_column}". Got columns: {dataframe.columns}')
+
+    ids: np.ndarray = dataframe.get_column(id_column).to_numpy()
+    unique_ids, counts = np.unique(ids, return_counts=True)
+    duplicated: np.ndarray = unique_ids[counts > 1]
+
+    if len(duplicated) > 0:
+        raise ValueError(f'Dataframe contains duplicate values in id_column {id_column}: {duplicated}')
+
+def _update_dataframe(current_dataframe: pl.DataFrame, updates: pl.DataFrame, id_column: str) -> pl.DataFrame:
+    """Update rows in current_dataframe with values from updates, matching on id_column.
+
+    If current_dataframe has rows that don't exist in updates as defined by the values in id_column, those
+    rows will stay as they are. The same goes for columns. If updates has columns that don't exist in current_dataframe,
+    those columns will be added. Any rows for which those columns are not defined in updates will be given a null value
+    for those columns. 
+
+    Parameters
+    ----------
+    current_dataframe: pl.DataFrame
+        The dataframe to update
+    updates: pl.DataFrame
+        The dataframe containing updated values
+    id_column: str
+        The column to match rows between dataframes
+
+    Returns
+    -------
+    pl.DataFrame
+        The updated dataframe
+
+    Raises
+    ------
+    ValueError
+        Raised when either dataframe does not have the id_column
+    ValueError  
+        Raised when either dataframe has duplicates in the id_column
+    ValueError
+        Raised when updates has values in id_column that don't exist in current_dataframe
+    """
+    _check_id_column_integrity(updates, id_column)
+    _check_id_column_integrity(current_dataframe, id_column)
+
+    new_ids: np.ndarray = updates.get_column(id_column).to_numpy()
+    old_ids: np.ndarray = current_dataframe.get_column(id_column).to_numpy()
+    unknown_ids = np.setdiff1d(new_ids, old_ids)
+
+    if len(unknown_ids) > 0:
+        raise ValueError(f'Updates contains id_column {id_column} values not found in current data: {unknown_ids}')
+
+    id_indices = np.where(current_dataframe.get_column(id_column).is_in(new_ids))[0]
+    updated_df = current_dataframe.clone()
+
+    for column in updates.columns:
+        if column not in updated_df.columns:
+            null_value = pl.lit(None).cast(updates.get_column(column).dtype)
+            updated_df = updated_df.with_columns(null_value.alias(column))
+
+        updated_df[id_indices, column] = updates.get_column(column)
+
+    return updated_df
+
 class KPMSProject:
     def __init__(self, project_dir_path: PathLike):
         """Initializes the KPMSProject, which serves as the 'Model' in MVC for kpms.
@@ -34,74 +114,17 @@ class KPMSProject:
 
         return pl.read_csv(self.recordings_csv_path)
 
-    @staticmethod
-    def _update_dataframe(current_dataframe: pl.DataFrame, updates: pl.DataFrame, id_column: str) -> pl.DataFrame:
-        """Update rows in current_dataframe with values from updates, matching on id_column.
+    def add_recordings(self, new_recordings: pl.DataFrame):
+        _check_id_column_integrity(new_recordings, 'name')
 
-        If current_dataframe has rows that don't exist in updates as defined by the values in id_column, those
-        rows will stay as they are. The same goes for columns. If updates has columns that don't exist in current_dataframe,
-        those columns will be added. Any rows for which those columns are not defined in updates will be given a null value
-        for those columns. 
+        if not self.recordings_csv_path.exists():
+            new_recordings.write_csv(self.recordings_csv_path)
+            return
 
-        Parameters
-        ----------
-        current_dataframe: pl.DataFrame
-            The dataframe to update
-        updates: pl.DataFrame
-            The dataframe containing updated values
-        id_column: str
-            The column to match rows between dataframes
+        recordings = pl.read_csv(self.recordings_csv_path)
+        recordings = pl.concat([recordings, new_recordings], how='diagonal_relaxed')
 
-        Returns
-        -------
-        pl.DataFrame
-            The updated dataframe
-
-        Raises
-        ------
-        ValueError
-            Raised when either dataframe does not have the id_column
-        ValueError  
-            Raised when either dataframe has duplicates in the id_column
-        ValueError
-            Raised when updates has values in id_column that don't exist in current_dataframe
-        """
-        if id_column not in updates.columns:
-            raise ValueError(f'Updates must have column "{id_column}". Got columns: {updates.columns}')
-
-        if id_column not in current_dataframe.columns:
-            raise ValueError(f'Current dataframe must have column "{id_column}". Got columns: {current_dataframe.columns}')
-
-        new_ids: np.ndarray = updates.get_column(id_column).to_numpy()
-        unique_new_ids, new_counts = np.unique(new_ids, return_counts=True)
-        duplicated_new: np.ndarray = unique_new_ids[new_counts > 1]
-
-        if len(duplicated_new) > 0:
-            raise ValueError(f'Updates contains duplicate values in id_column {id_column}: {duplicated_new}')
-
-        old_ids: np.ndarray = current_dataframe.get_column(id_column).to_numpy()
-        unique_old_ids, old_counts = np.unique(old_ids, return_counts=True)
-        duplicated_old: np.ndarray = unique_old_ids[old_counts > 1]
-
-        if len(duplicated_old) > 0:
-            raise ValueError(f'Current dataframe contains duplicate values in id_column {id_column}: {duplicated_old}')
-
-        unknown_ids = np.setdiff1d(new_ids, old_ids)
-
-        if len(unknown_ids) > 0:
-            raise ValueError(f'Updates contains {id_column} values not found in current data: {unknown_ids}')
-
-        id_indices = old_ids[np.where(current_dataframe.get_column(id_column).is_in(new_ids))]
-        updated_df = current_dataframe.clone()
-
-        for column in updates.columns:
-            if column not in updated_df.columns:
-                null_value = pl.lit(None).cast(updates.get_column(column).dtype)
-                updated_df = updated_df.with_columns(null_value.alias(column))
-
-            updated_df[id_indices, column] = updates.get_column(column)
-
-        return updated_df
+        recordings.write_csv(self.recordings_csv_path)
 
     def update_recordings(self, new_recordings: pl.DataFrame):
         """Update the project recordings CSV with new or updated features of each recording.
@@ -119,12 +142,12 @@ class KPMSProject:
         RuntimeError
             Raised when the recordings CSV does not exist.
         ValueError
-            Raised when new_recordings has invalid data (see _update_dataframe)
+            Raised when new_recordings has invalid data (see update_dataframe)
         """
         if not self.recordings_csv_path.exists():
             raise RuntimeError(f'{self.recordings_csv_path} does not exist.')
 
-        updated_recordings = self._update_dataframe(
+        updated_recordings = _update_dataframe(
             self.get_recordings(),
             new_recordings,
             'name'
